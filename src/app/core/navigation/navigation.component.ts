310f79afc293b0f431842c593da33e7591186e0c
Hossein heydarpour <hosseinheydarpour1@gmail.com>
2025-08-24 18:06:59 +0330

    feat: add navigation bar

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-navigation',
-  standalone: true,
-  imports: [],
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+	selector: 'app-navigation',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './navigation.component.html',
+	styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
+	ifSearchExpanded = signal(false);
 
+	onExpandSearch() {
+		this.ifSearchExpanded.set(!this.ifSearchExpanded());
+	}
 }
